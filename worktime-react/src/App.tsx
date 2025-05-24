4489cb61b236829cfd39b65bc4860a377ebbe897
odmi-n <odamin619@gmail.com>
2025-05-24 17:53:03 +0900

    Revert "shitfreee.htmlにリダイレクトされない現象の改善。"
    This reverts commit 3db90387abc9c08fb4e332d083a1283e610a924f.

--- worktime-react/src/App.tsx
+++ worktime-react/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WorkTimeCalculator from './pages/WorkTimeCalculator';
 import AttendanceTracker from './pages/AttendanceTracker';
 

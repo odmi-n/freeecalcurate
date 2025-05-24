3db90387abc9c08fb4e332d083a1283e610a924f
odmi-n <odamin619@gmail.com>
2025-05-24 17:35:15 +0900

    shitfreee.htmlにリダイレクトされない現象の改善。

--- worktime-react/src/App.tsx
+++ worktime-react/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import WorkTimeCalculator from './pages/WorkTimeCalculator';
 import AttendanceTracker from './pages/AttendanceTracker';
 
